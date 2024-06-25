f0286611e494f6adb10c19089b09273769580b50
DigiCris <cmarchesetdiii@gmail.com>
2024-06-25 00:49:31 -0300

    last block in blockchain by sending RPC

--- blockchain.go
+++ blockchain.go
@@ -2,13 +2,13 @@ package blockchain
 
 import (
     "fmt"
-//    "log"
+    "log"
 
-    //"github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/ethclient"
 )
 
-func GetLatestBlockInfo() {/*
-    client, err := ethclient.Dial("https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID")
+func GetLatestBlockInfo(rpc string) {
+    client, err := ethclient.Dial(rpc)
     if err != nil {
         log.Fatal(err)
     }
@@ -21,6 +21,4 @@ func GetLatestBlockInfo() {/*
     fmt.Printf("Last block number: %d\n", block.Number())
     fmt.Printf("Last block hash: %x\n", block.Hash())
     fmt.Printf("Last block timestamp: %d\n", block.Time())
-	*/
-	fmt.Println("Hello World")
 }
